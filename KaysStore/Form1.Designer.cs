ef929efe628797e81195bcc87227cc83d729a661
Kay <Kay@LAPTOP-R26J0GK6>
2019-09-25 20:49:31 +0100

    added footer to admin page

--- KaysStore/Form1.Designer.cs
+++ KaysStore/Form1.Designer.cs
@@ -28,22 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlFooter = new System.Windows.Forms.Panel();
+            this.lblFooter = new System.Windows.Forms.Label();
+            this.pnlFooter.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnlFooter
+            // 
+            this.pnlFooter.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnlFooter.Controls.Add(this.lblFooter);
+            this.pnlFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlFooter.Location = new System.Drawing.Point(0, 277);
+            this.pnlFooter.Name = "pnlFooter";
+            this.pnlFooter.Size = new System.Drawing.Size(1335, 45);
+            this.pnlFooter.TabIndex = 0;
+            // 
+            // lblFooter
+            // 
+            this.lblFooter.AutoSize = true;
+            this.lblFooter.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFooter.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblFooter.Location = new System.Drawing.Point(865, 12);
+            this.lblFooter.Name = "lblFooter";
+            this.lblFooter.Size = new System.Drawing.Size(197, 24);
+            this.lblFooter.TabIndex = 0;
+            this.lblFooter.Text = "Developed by Kay Mo";
             // 
             // frmAdminDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1335, 322);
+            this.Controls.Add(this.pnlFooter);
             this.Name = "frmAdminDashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Admin Dashboard";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.pnlFooter.ResumeLayout(false);
+            this.pnlFooter.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlFooter;
+        private System.Windows.Forms.Label lblFooter;
     }
 }
 
